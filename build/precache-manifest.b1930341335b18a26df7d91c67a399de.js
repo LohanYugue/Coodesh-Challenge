db2442aa1b267bef2b998827c3a64e64c3902521
LohanYugue <lohanyugue.mandic@natura.net>
2022-12-30 17:55:53 -0300

    Add build

---
+++ build/precache-manifest.b1930341335b18a26df7d91c67a399de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c8447786fe3cceb6fefa805c245401",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "5d4378cd631caf407b50",
+    "url": "/calculator/static/css/main.86f680a8.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "5d4378cd631caf407b50",
+    "url": "/calculator/static/js/main.188c5c6f.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
